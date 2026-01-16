1a0b055550521251544293816baec169edfe4ea7
Shaun Taheri <gpg@taheris.net>
2026-01-16 11:53:11 +0000

    Run nix fmt on builder code

--- lib/builder/hostkey.nix
+++ lib/builder/hostkey.nix
@@ -8,17 +8,19 @@
 #
 { pkgs }:
 
-pkgs.runCommand "wrapix-builder-keys" {
-  nativeBuildInputs = [ pkgs.openssh ];
-} ''
-  mkdir -p $out
+pkgs.runCommand "wrapix-builder-keys"
+  {
+    nativeBuildInputs = [ pkgs.openssh ];
+  }
+  ''
+    mkdir -p $out
 
-  # Generate host key (for server identity)
-  ssh-keygen -t ed25519 -f $out/ssh_host_ed25519_key -N "" -C "wrapix-builder-host" </dev/null
+    # Generate host key (for server identity)
+    ssh-keygen -t ed25519 -f $out/ssh_host_ed25519_key -N "" -C "wrapix-builder-host" </dev/null
 
-  # Generate client key (for SSH authentication to builder)
-  ssh-keygen -t ed25519 -f $out/builder_ed25519 -N "" -C "wrapix-builder-client" </dev/null
+    # Generate client key (for SSH authentication to builder)
+    ssh-keygen -t ed25519 -f $out/builder_ed25519 -N "" -C "wrapix-builder-client" </dev/null
 
-  # Create base64-encoded public host key for nix-darwin buildMachines
-  base64 < $out/ssh_host_ed25519_key.pub | tr -d '\n' > $out/public_host_key_base64
-''
+    # Create base64-encoded public host key for nix-darwin buildMachines
+    base64 < $out/ssh_host_ed25519_key.pub | tr -d '\n' > $out/public_host_key_base64
+  ''
